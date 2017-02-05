43c4e5fc2ff42ba05a95338add31cb87eae4b088
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-02-06 00:48:03 +0100

    Updated build system.

--- libfl8/premake5.lua
+++ libfl8/premake5.lua
@@ -14,18 +14,19 @@ workspace (name)
     filter {"configurations:Release"}
         defines {"RELEASE"}
         optimize "Speed"
+        symbols "Off"
 
 -- The Library
 project (name)
     targetdir "lib"
+    files {"src/fl8/**.cc"}
+    includedirs {"include"}
     filter {"platforms:Static"}
         defines {"STATIC"}
         kind "StaticLib"
     filter {"platforms:Shared"}
         defines {"SHARED"}
         kind "SharedLib"
-    files {"src/fl8/**.cc"}
-    includedirs {"include"}
 
 -- Library Testing Suite
 project (name.."-tests")
