b2e6b400ec83a3ddab353bcdfa337a62aaacb728
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-02-19 19:29:13 +0100

    Started working on dictionary.

--- liblzw/include/lzw/definitions.hh
+++ liblzw/include/lzw/definitions.hh
@@ -10,13 +10,14 @@ namespace lzw {
     // be changed by the user, but carefully, since we need 2 x more index space.
     static constexpr std::size_t WORD_BUFFER_SIZE { 1024 }, // Restriciton below.
                                  CODE_BUFFER_SIZE { 2048 }; // Gives 4 KiB pages.
+    static constexpr std::size_t DICTIONARY_ITEMS { 2 << 16 }; // 65536 elements.
     static constexpr std::size_t BYTE_SIZE_IN_BITS { CHAR_BIT }; // Edge cases...
-    static constexpr std::size_t DICTIONARY_SIZE { (2 << 16) - 1 }; // 16K items.
     using Byte = unsigned char; // Since the bitsize of characters isn't actually
     // guaranteed to be always 8 bits in all platforms, we fail to compile these.
     using Index = unsigned short; // According to the standard, shorts are always
     // 16-bits long. Therefore, we don't need to make any checks for this type...
     using String = Byte*; // Is used for storing the sequences in the dictionary.
+    static constexpr std::size_t ENTRY_SIZE { sizeof(Byte) + 4 * sizeof(Index) };
     static_assert(BYTE_SIZE_IN_BITS == 8, "Your platform is an edge case. Sry.");
 }
 
--- liblzw/include/lzw/dictionary.hh
+++ liblzw/include/lzw/dictionary.hh
@@ -4,6 +4,25 @@
 #include "lzw/definitions.hh"
 
 namespace lzw {
+    class Dictionary final {
+    public:
+        Dictionary(Byte* heads, Index* indices,
+                   Index* rights, Index* lefts,
+                   Index* roots, std::size_t size)
+            : heads { heads }, indices { indices },
+              rights { rights }, lefts { lefts },
+              roots { roots }, size { size } {  }
+
+    protected:
+    private:
+        Byte* heads { nullptr }; // Contains the parts after the <prefix>, the <head>.
+        Index* indices { nullptr }; // Is needed since we need to know parent indices.
+        Index* rights { nullptr }, // Stores the right node of the binary search tree.
+               *lefts { nullptr }, // Same deal here, but here we have the left nodes.
+               *roots { nullptr }; // Finally, the root of the tree which is <prefix>.
+        std::size_t size { 0 },
+                    head { 0 }; // Enables easier insertion of new dictionary entires.
+    };
 }
 
 #endif
