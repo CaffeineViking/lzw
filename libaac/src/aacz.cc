51306957c9dac3844b0fe663675f26653a025f98
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-03-06 19:36:55 +0100

    Finished writing compressor.

--- libaac/src/aacz.cc
+++ libaac/src/aacz.cc
@@ -30,18 +30,55 @@ int main(int argc, char** argv) {
 
     aac::Byte byte { 0 };
     std::size_t head { 0 };
-    aac::Byte bit_buffer[aac::MAX_PENDING] { 0 };
-    input_file.read(&byte, sizeof(aac::Byte));
+    aac::Byte bit_buffer[aac::MAX_PENDING];
+    input_file.read((char*)&byte, sizeof(aac::Byte));
 
     while (input_file.gcount()) {
+        std::cout << "Encoding: " << byte << std::endl;
         std::size_t bits_written { ascii_encoder.step(byte, bit_buffer, head) };
-        head += bits_written; // We might have to delay them writing until full.
-        if (head >> 3 != 0) { // That is, until we have written at least a byte.
+        std::cout << "Created: " << bits_written << " bits" << std::endl;
+        head += bits_written; // We might have to delay writing them until full.
+        std::cout << "Total: " << head << " bits" << std::endl;
+        if ((head >> 3) != 0) { // That is, until we have written at least a byte.
             std::size_t bytes_written { head >> 3 };
             std::size_t bits_remaining { head - (bytes_written << 3) };
+            std::cout << "Writing: " << bytes_written << " bytes" << std::endl;
+            output_file.write((char*)bit_buffer, bytes_written);
+
+            // Copy the remaining bits to head of the buffer.
+            for (std::size_t i { 0 }; i < bits_remaining; ++i) {
+                int b { (bit_buffer[bytes_written] >> (7 - i)) & 0x01 };
+                bit_buffer[0] &= ~(1 << (7 - i));
+                bit_buffer[0] |= b << (7 - i);
+            }
+
+            std::cout << "Bits remaining: " << bits_remaining << std::endl;
+            std::cout << "Head of buffer: " << std::hex << (int)bit_buffer[0]
+                                            << std::dec << std::endl;
+            head = bits_remaining; // Next bits will be put after.
         }
 
-        input_file.read(&byte, sizeof(aac::Byte));
+        std::cout << std::endl;
+        input_file.read((char*)&byte, sizeof(aac::Byte));
+    }
+
+    ascii_encoder.exit(bit_buffer, head);
+    std::size_t bytes_written { head >> 3 };
+    output_file.write((char*)bit_buffer, bytes_written);
+    std::size_t bits_remaining { head - (bytes_written << 3) };
+
+    if (bits_remaining > 0) { // Still stuff to copy.
+        // Copy the remaining bits to head of the buffer.
+        for (std::size_t i { 0 }; i < bits_remaining; ++i) {
+            bool b { (bit_buffer[bytes_written] >> (7 - i)) & 0x01 };
+            bit_buffer[0] &= ~(1 << (7 - i));
+            bit_buffer[0] |= b << (7 - i);
+        }
+
+        // Finally, clear any padding bits added to bytes.
+        for (std::size_t i { bits_remaining }; i < 7; ++i)
+            bit_buffer[0] &= ~(1 << (7 - i));
+        output_file.write((char*)bit_buffer, 1);
     }
 
     return 0;
