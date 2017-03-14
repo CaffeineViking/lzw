4d0752bb581c765e08b63cb55022e68329d2c047
Erik S. V. Jansson <caffeineviking@gmail.com>
2017-03-14 13:29:50 +0100

    Completed article.

--- report/share/libaac/decoder.cc
+++ report/share/libaac/decoder.cc
@@ -1,9 +1,53 @@
 #include "aac/decoder.hh"
 
+#include <iostream>
+
 std::size_t aac::Decoder::step(Counter codew, Byte* buffer,
                                std::size_t buffer_bitnum) {
     std::size_t bits_written { 0 };
-    Range length { (Range)upper - (Range)lower + 1 };
+    number = number << 8; number += codew;
+
+    Range length { (Bound)upper - (Bound)lower + 1 };
+    Bound unscaled { (Bound)number - (Bound)lower + 1 };
+    unscaled *= statistics.total() - 1;
+    unscaled /= length;
+
+    Counter symbol { statistics.find(unscaled) };
+    if (symbol==END_OF_FILE) return bits_written;
+    buffer[buffer_bitnum << 3] = symbol;
+    bits_written += 8;
+
+    statistics.update(symbol);
+    Counter total_symbols { statistics.total() };
+    Interval symbol_interval { statistics.symbol(symbol) };
+    Bound rescale { ((Bound)length * (Bound)symbol_interval.second) };
+    rescale /= (Bound)total_symbols;
+    upper = lower + rescale - 1;
+
+    rescale = ((Bound)length * (Bound)symbol_interval.first);
+    rescale /= (Bound)total_symbols;
+    lower = lower + rescale;
+
+    while (true) {
+        // Matching most significant bit for lower and upper.
+        if ((lower & SIGNIFICANT) == (upper & SIGNIFICANT)) {
+        // Non-matching most significant bit and also non-matching
+        // second most significant... We will never converge here.
+        // Therefore, handle this case by letting some bit buffer.
+        } else if ((lower & SECOND_MOST) && !(upper & SECOND_MOST)) {
+            // Remove the SMSB from the stream...
+            lower = lower & ~(SIGNIFICANT | SECOND_MOST);
+            upper = upper | SECOND_MOST;
+            number ^= SECOND_MOST;
+        // Everything is ok.
+        } else break;
+
+        lower = lower << 1;
+        upper = upper << 1;
+        upper = upper | 1;
+    }
+
+    return bits_written;
 }
 
 std::size_t aac::Decoder::write(bool state, Byte* buffer,
